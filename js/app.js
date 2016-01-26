95172989d27851377d1c0e385b55bb04c99f3e89
jmllegrand <jml.legrand@gmail.com>
2016-01-26 22:57:08 +0100

    neighborhoodMap: add ifee (and failure)

--- js/app.js
+++ js/app.js
@@ -2,23 +2,21 @@
  * Created by jmlegrand on 26/01/16.
  */
 
+(function () {
+    function initMap() {
+      var myLatlng = new google.maps.LatLng(48.89067, 2.41592);
 
+      var map = new google.maps.Map(document.getElementById('map'), {
+        center: myLatLng,
+        zoom: 12
+      });
 
-var myLatLng = {
-  lat: 48.89067,
-  lng: 2.41592
-};
+      var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: 'Hello World!'
+      });
+    }
 
-function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: myLatLng,
-    zoom: 12
-  });
-
-  var marker = new google.maps.Marker({
-    position: myLatLng,
-    map: map,
-    title: 'Hello World!'
-  });
-
-}
+    initMap();
+  })();
