a835926f6474f698f7085cd32cd227e1366462f6
jmllegrand <jml.legrand@gmail.com>
2016-01-26 21:40:09 +0100

    neighborhoodMap: init version

---
+++ js/app.js
@@ -0,0 +1,11 @@
+/**
+ * Created by jmlegrand on 26/01/16.
+ */
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+}
