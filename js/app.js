9e77451ea8e30e92ca807256a1b731e5d309f34b
jmllegrand <jml.legrand@gmail.com>
2016-01-27 19:31:15 +0100

    neighborhoodMap: add places (step 4)

--- js/app.js
+++ js/app.js
@@ -2,21 +2,25 @@
  * Created by jmlegrand on 26/01/16.
  */
 
-(function () {
-    function initMap() {
-      var myLatlng = new google.maps.LatLng(48.89067, 2.41592);
 
-      var map = new google.maps.Map(document.getElementById('map'), {
-        center: myLatLng,
-        zoom: 12
-      });
 
-      var marker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-        title: 'Hello World!'
-      });
-    }
+var myLatLng = {
+  lat: 48.89067,
+  lng: 2.41592
+};
 
-    initMap();
-  })();
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: myLatLng,
+    zoom: 12
+  });
+
+  datas.forEach(function (touristPlace) {
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(touristPlace.lat, touristPlace.lng),
+      map: map,
+      title: 'Hello World!'
+    });
+    marker.setMap(map);
+  })
+}
---
+++ js/datas.js
@@ -0,0 +1,22 @@
+// mocked json file
+var datas = [{
+  "lat": "48.86342",
+  "lng": "2.26189",
+  "title": 'stade roland garros'
+}, {
+  "lat": "48.86418",
+  "lng": "2.34250",
+  "title": 'louvre'
+}, {
+  "lat": "48.85826",
+  "lng": "2.29451",
+  "title": 'tour eiffel'
+}, {
+  "lat": "48.84622",
+  "lng": "2.33716",
+  "title": 'jardin du luxembourg'
+}, {
+  "lat": "48.85273",
+  "lng": "2.35056",
+  "title": 'notre dame'
+}];
