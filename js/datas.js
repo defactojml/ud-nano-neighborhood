375d80964bcd794d6099843f203c92b6ce893781
jean-michel.legrand <Fluent2015>
2016-02-11 19:26:10 +0100

    neighborhoodMap: minor refactoring

--- js/datas.js
+++ js/datas.js
@@ -1,55 +1,55 @@
 // mocked json file
 var datas = [
   {
-    "lat": "48.86342",
-    "lng": "2.26189",
-    "title": 'Roland Garros'
+    lat: "48.86342",
+    lng: "2.26189",
+    title: 'Roland Garros'
   }, {
-    "lat": "48.86418",
-    "lng": "2.34250",
-    "title": 'Le Louvre'
+    lat: "48.86418",
+    lng: "2.34250",
+    title: 'Le Louvre'
   }, {
-    "lat": "48.85826",
-    "lng": "2.29451",
-    "title": 'La Tour Eiffel'
+    lat: "48.85826",
+    lng: "2.29451",
+    title: 'La Tour Eiffel'
   }, {
-    "lat": "48.84622",
-    "lng": "2.33716",
-    "title": 'Le Jardin du Luxembourg'
+    lat: "48.84622",
+    lng: "2.33716",
+    title: 'Le Jardin du Luxembourg'
   }, {
-    "lat": "48.85273",
-    "lng": "2.35056",
-    "title": 'Notre Dame de Paris'
+    lat: "48.85273",
+    lng: "2.35056",
+    title: 'Notre Dame de Paris'
   },
   {
-    "lat": "48.804865",
-    "lng": "2.120355",
-    "title": 'Chateau de Versailles'
+    lat: "48.804865",
+    lng: "2.120355",
+    title: 'Chateau de Versailles'
   },
   {
-    "lat": "48.833832",
-    "lng": "2.332422",
-    "title": 'Catacombes de Paris'
+    lat: "48.833832",
+    lng: "2.332422",
+    title: 'Catacombes de Paris'
   },
   {
-    "lat": "48.873779",
-    "lng": "2.295016",
-    "title": 'Arc de Triomphe'
+    lat: "48.873779",
+    lng: "2.295016",
+    title: 'Arc de Triomphe'
   },
   {
-    "lat": "48.871953",
-    "lng": "2.775924",
-    "title": 'Disneyland Paris'
+    lat: "48.871953",
+    lng: "2.775924",
+    title: 'Disneyland Paris'
   },
   {
-    "lat": "48.842568",
-    "lng": "2.435494",
-    "title": 'Chateau de Vincennes'
+    lat: "48.842568",
+    lng: "2.435494",
+    title: 'Chateau de Vincennes'
   }];
 
 
 var mapsOptions = {
-  center : {
+  center: {
     lat: 48.852729,
     lng: 2.350564
   },
